0e77a0c5d3ea86886e74ee1b87ee46454cc4e8ec
EwyBoy <ewyboy@gmail.com>
2015-11-28 03:46:41 +0100

    Fixed MC not starting

--- src/main/java/com/quentillionaire/dddminimap/Utillity/StringMap.java
+++ src/main/java/com/quentillionaire/dddminimap/Utillity/StringMap.java
@@ -3,8 +3,8 @@ package com.quentillionaire.dddminimap.Utillity;
 public class StringMap {
 
     //Mod info
-    public static final String ID = "hammertime";
-    public static final String Name = "Hammer Time";
+    public static final String ID = "dddminimap";
+    public static final String Name = "3D Minimap";
     public static final String MinecraftVersion = "1.7.10";
     public static final String VersionMajor = "1";
     public static final String VersionMinor = "3";
@@ -12,6 +12,6 @@ public class StringMap {
     public static final String VersionBuildName = Name + "-" + MinecraftVersion + "-" + VersionMajor + "." + VersionMinor + "." + VersionPatch;
 
     //Proxy Paths
-    public static final String clientProxyPath = "com.ewyboy.hammertime.Networking.ClientProxy";
-    public static final String serverProxyPath = "com.ewyboy.hammertime.Networking.CommonProxy";
+    public static final String clientProxyPath = "com.quentillionaire.dddminimap.Networking.ClientProxy";
+    public static final String serverProxyPath = "com.quentillionaire.dddminimap.Networking.CommonProxy";
 }
