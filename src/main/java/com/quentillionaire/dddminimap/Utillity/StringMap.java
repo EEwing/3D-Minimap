89b5cd00224cc6a1999292c93eb7669b18d43bcc
eewing <eewing@cs.uoregon.edu>
2015-11-27 18:50:04 -0800

    Updated Version Number

--- src/main/java/com/quentillionaire/dddminimap/Utillity/StringMap.java
+++ src/main/java/com/quentillionaire/dddminimap/Utillity/StringMap.java
@@ -7,8 +7,8 @@ public class StringMap {
     public static final String Name = "3D Minimap";
     public static final String MinecraftVersion = "1.7.10";
     public static final String VersionMajor = "1";
-    public static final String VersionMinor = "3";
-    public static final String VersionPatch = "2";
+    public static final String VersionMinor = "0";
+    public static final String VersionPatch = "0";
     public static final String VersionBuildName = Name + "-" + MinecraftVersion + "-" + VersionMajor + "." + VersionMinor + "." + VersionPatch;
 
     //Proxy Paths
