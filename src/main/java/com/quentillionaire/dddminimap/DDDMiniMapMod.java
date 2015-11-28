b3a6166b6be22483a7dd28a03b1d861d44de5858
EwyBoy <EwyBoy@gmail.com>
2015-11-28 01:56:46 +0100

    Update DDDMiniMapMod.java
    Added main class

--- src/main/java/com/quentillionaire/dddminimap/DDDMiniMapMod.java
+++ src/main/java/com/quentillionaire/dddminimap/DDDMiniMapMod.java
@@ -1,5 +1,43 @@
 package com.quentillionaire.dddminimap;
 
+@Mod(modid = StringMap.ID, name = StringMap.Name, version = StringMap.VersionBuildName, acceptedMinecraftVersions = "["+StringMap.MinecraftVersion+"]")
 public class DDDMiniMapMod {
+  
+@Mod.Instance(StringMap.ID)
+public static DDDMiniMapMod instance;
 
+@SidedProxy(modId = StringMap.ID, clientSide = StringMap.clientProxyPath, serverSide = StringMap.commonProxyPath)
+public static CommonProxy proxy;
+
+private long launchTime;
+
+@Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        Stopwatch watch = Stopwatch.createStarted();
+          Logger.info("Pre-Initialization started");
+            //ConfigLoader.init(event.getSuggestedConfigurationFile());
+            launchTime += watch.elapsed(TimeUnit.MILLISECONDS);
+          Logger.info("Pre-Initialization finished after " + watch.elapsed(TimeUnit.MILLISECONDS) + "ms");
+        Logger.info("Pre-Initialization process successfully done");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        Stopwatch watch = Stopwatch.createStarted();
+          Logger.info("Initialization started");
+            launchTime += watch.elapsed(TimeUnit.MILLISECONDS);
+          Logger.info("Initialization finished after " + watch.elapsed(TimeUnit.MILLISECONDS) + "ms");
+        Logger.info("Initialization process successfully done");
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        Stopwatch watch = Stopwatch.createStarted();
+          Logger.info("Post-Initialization started");
+            launchTime += watch.elapsed(TimeUnit.MILLISECONDS);
+          Logger.info("Post-Initialization finished after " + watch.elapsed(TimeUnit.MILLISECONDS) + "ms");
+          Logger.info("Post-Initialization process successfully done");
+        Logger.info("Total launch time for " + StringMap.Name + " : " + launchTime + " ms");
+    }
 }
+
